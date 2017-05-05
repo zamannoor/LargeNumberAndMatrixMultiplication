d48b2b652210ad5625107b38d90ec768b1048fb5
Hasanuzzaman Noor <zamannoor@users.noreply.github.com>
2017-05-05 10:06:10 -0500

    Update Controller.java

--- src/Controller.java
+++ src/Controller.java
@@ -1,35 +1,57 @@
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 
 
 public class Controller {
   
-  Map<Integer, BigIntegers> input = new HashMap<Integer, BigIntegers>();
-  Map<Integer, BigIntegers> output = new HashMap<Integer, BigIntegers>();
-  int row, col;
+  HashMap<Integer, BigIntegers> input1 = new HashMap<Integer, BigIntegers>();
+  HashMap<Integer, BigIntegers> input2 = new HashMap<Integer, BigIntegers>();
+  int row;
 
   void takeInput() {
     try {
       String line;
       BufferedReader br = new BufferedReader(new FileReader("input.txt"));
       int i =0;
-     
+      int input1Len =0;
+      int input2Len =0;
+      
       while ((line = br.readLine()) != null) {  
         String[] strs = line.trim().split("\\s+");
         if (i ==0){
           row=Integer.parseInt(strs[0]);
-          col =Integer.parseInt(strs[1]);
-        } else if (strs.length == 3){
-          int x,y;
-          x = Integer.parseInt(strs[0]);
-          y = Integer.parseInt(strs[1]);
-         input.put(x*col+y, new BigIntegers(strs[2]));
+        } else if (i ==1 ) {
+          input1Len = Integer.parseInt(strs[0]);
+          
+          for (int j=0 ;j<input1Len; j++) {
+            line = br.readLine();
+            if (line != null) {
+              String[] strs2 = line.trim().split("\\s+");
+              int x,y;
+              x = Integer.parseInt(strs2[0]);
+              y = Integer.parseInt(strs2[1]);
+              input1.put(x*row+y, new BigIntegers(strs2[2]));
+            }
+          }
+          
+        } else{
+          input2Len = Integer.parseInt(strs[0]);
+          for (int j=0 ;j<input2Len; j++) {
+            line = br.readLine();
+            if (line != null) {
+              String[] strs2 = line.trim().split("\\s+");
+              int x,y;
+              x = Integer.parseInt(strs2[0]);
+              y = Integer.parseInt(strs2[1]);
+              input2.put(x*row+y, new BigIntegers(strs2[2]));
+            }
+          }
+          break;
         }
         i++;
       }   
@@ -43,26 +65,23 @@ public class Controller {
   
   
   
-  public static void main(String[] args) throws IOException {
-    
-    
+  public static void main(String[] args) throws FileNotFoundException {
     Controller controller = new Controller();
     controller.takeInput();
-    
     MatrixMultiplication mm = new MatrixMultiplication(controller, 
         controller.row, 
-        controller.col, 
-        controller.input,
-        controller.output);
-
-    mm.matrixMultiplyByStrassen();
-    mm.traditionalMatrixMultiplication();
+        controller.input1,
+        controller.input2);
     
-
-    UnitTestBigInteger unitTest = new UnitTestBigInteger();
-    unitTest.unitTestAddition();
-    unitTest.unitTestSubtraction();
-    unitTest.unitTestMultiplication();
+    //mm.naiveMatrixMultiplicationWithSchool();
     
+    // 1 means naive
+    // 2 means gauss
+    // 3 means karatsuba
+    
+    mm.matrixMultStrassen(1);
+    mm.matrixMultStrassen(2);
+    mm.matrixMultStrassen(3);
+    System.out.println("Done!");
   }
 }
